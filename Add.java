ad75127367b7afe9a62ef38b99fc80e6099d1914
Charan <charansai2808@gmail.com>
2023-11-28 12:06:45 +0530

    Changed new

--- Add.java
+++ Add.java
@@ -2,7 +2,7 @@ import java.util.Scanner; // Import the Scanner class
 
 class MyClass {
   public static void main(String[] args) {
-    int x, y, sum;
+    int x, y, addition;
     Scanner myObj = new Scanner(System.in); // Create a Scanner object
     System.out.println("Type a number:");
     x = myObj.nextInt(); // Read user input
@@ -10,7 +10,7 @@ class MyClass {
     System.out.println("Type another number:");
     y = myObj.nextInt(); // Read user input
 
-    sum = x + y;  // Calculate the sum of x + y
-    System.out.println("Sum is: " + sum); // Print the sum
+    addition = x + y;  // Calculate the sum of x + y
+    System.out.println("Sum is: " + addition); // Print the sum
   }
 } 
